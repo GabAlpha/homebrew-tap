6ad5ac9194a6ace4f58c5dfc66d3772857bfe54a
Gabriele Alfarano <51409681+GabAlpha@users.noreply.github.com>
2024-10-03 22:47:23 +0200

    update basilk formula

--- Formula/basilk.rb
+++ Formula/basilk.rb
@@ -9,14 +9,14 @@ class Basilk < Formula
       end
       on_intel do
         url "https://github.com/GabAlpha/basilk/releases/download/0.2.0/basilk-x86_64-apple-darwin.tar.gz"
-        sha256 "1149ee8d28ac9df1f0f625ddbf0e2d16e57d835811186f95faafecb6dcfbb529"
+        sha256 "1bb16be5c147e08413f6defd810602f3df6d69bba0f7a105396a9c2499a87b12"
       end
     end
 
     on_linux do
       on_intel do
         url "https://github.com/GabAlpha/basilk/releases/download/0.2.0/basilk-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "2d1518a72b861bb94de54a71c64bf2b7060e382673ea4e679a5f494eb535e3bc"
+        sha256 "f2eccfe13087b5beb1f556294f799b0a800ceaf2dc5c258b921e814dc4b851f8"
       end
     end
     
