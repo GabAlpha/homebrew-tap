f5213d44e72cd41e6119b602a39670b950fc0149
Gabriele Alfarano <51409681+GabAlpha@users.noreply.github.com>
2025-03-23 17:23:30 +0100

    Update basilk.rb

--- Formula/basilk.rb
+++ Formula/basilk.rb
@@ -4,19 +4,19 @@ class Basilk < Formula
   
     on_macos do
       on_arm do
-        url "https://github.com/GabAlpha/basilk/releases/download/0.2.0/basilk-aarch64-apple-darwin.tar.gz"
-        sha256 "f073e37f93df22c1e35c576b9bbb10ce4b5e6d6364b28831e7be4cd47f8cb662"
+        url "https://github.com/GabAlpha/basilk/releases/download/0.2.1/basilk-aarch64-apple-darwin.tar.gz"
+        sha256 "24bce5de32bff466f84df6daf8ea7b838423847e608297aac94ab7ca2ad948ee"
       end
       on_intel do
-        url "https://github.com/GabAlpha/basilk/releases/download/0.2.0/basilk-x86_64-apple-darwin.tar.gz"
-        sha256 "1bb16be5c147e08413f6defd810602f3df6d69bba0f7a105396a9c2499a87b12"
+        url "https://github.com/GabAlpha/basilk/releases/download/0.2.1/basilk-x86_64-apple-darwin.tar.gz"
+        sha256 "fb4097300aeac9a0ba0ad0854733a26df88be3e244d9199f3dda34418438d1dc"
       end
     end
 
     on_linux do
       on_intel do
-        url "https://github.com/GabAlpha/basilk/releases/download/0.2.0/basilk-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "f2eccfe13087b5beb1f556294f799b0a800ceaf2dc5c258b921e814dc4b851f8"
+        url "https://github.com/GabAlpha/basilk/releases/download/0.2.1/basilk-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "8ad1a251089c3ad3757d1414081278e769ac93b907712e90acf0bc16c1acbd6c"
       end
     end
     
